34dc8317db74503c98b4ab316480f84c2f3c42e7
Hugo.wu <hugo.wu@ringcentral.com>
2023-06-03 17:57:15 +0800

    misc: Move logic to WechatService

--- src/main/java/com/bear/crawler/webmagic/controller/WebMagicController.java
+++ src/main/java/com/bear/crawler/webmagic/controller/WebMagicController.java
@@ -1,14 +1,5 @@
 package com.bear.crawler.webmagic.controller;
 
-import cn.hutool.core.collection.CollectionUtil;
-import com.bear.crawler.webmagic.dao.WArticleDao;
-import com.bear.crawler.webmagic.mybatis.generator.po.WAccountPO;
-import com.bear.crawler.webmagic.pojo.WechatConfig;
-import com.bear.crawler.webmagic.processor.WebMagicTestProcessor;
-import com.bear.crawler.webmagic.processor.WAccountProcessor;
-import com.bear.crawler.webmagic.processor.WArticleDetailProcessor;
-import com.bear.crawler.webmagic.processor.WArticleProcessor;
-import com.bear.crawler.webmagic.provider.WAccountProvider;
 import com.bear.crawler.webmagic.service.WechatService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import us.codecraft.webmagic.Request;
-import us.codecraft.webmagic.Spider;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RequestMapping("/webMagic")
@@ -31,100 +17,42 @@ import java.util.Map;
 public class WebMagicController {
 
     @Autowired
-    private WechatConfig wechatConfig;
-
-    @Autowired
-    private WebMagicTestProcessor webMagicTestProcessor;
-
-    @Autowired
-    private WArticleDetailProcessor wArticleDetailProcessor;
-
-    @Autowired
-    private WArticleProcessor wArticleProcessor;
-
-    @Autowired
-    private WAccountProcessor wAccountProcessor;
-
-    @Autowired
-    private WAccountProvider wAccountProvider;
-
-    @Autowired
-    private WArticleDao wArticleDao;
-
-    @Autowired
     private WechatService wechatService;
 
-    @GetMapping("/testWebMagic")
-    public String testWebMagic() {
-        wArticleDao.selectLatest("FakeId");
-//        Spider.create(webMagicTestProcessor)
-//                .addUrl("https://mp.weixin.qq.com/s/BeYIAdsEZ6TVzFaOO3C-Pw")
-//                .thread(5)
-//                .run();
-        // 会等processor处理完之后才回调testWebMagic success
-        return "testWebMagic success";
-    }
-
-    @GetMapping("/loadWArticleDetail")
-    public String loadWArticleDetail(@RequestParam("query") String query) {
-        Spider.create(wArticleDetailProcessor)
-                .addUrl("https://mp.weixin.qq.com/s/" + query)
-                .thread(5)
-                .run();
-        return "loadWArticleDetail success";
+    @GetMapping("/fetchWArticleDetail")
+    public String fetchWArticleDetail(@RequestParam("query") String query) {
+        wechatService.fetchWArticleDetail(query);
+        return "fetchWArticleDetail success";
     }
 
     // TODO: 5/21/23 spider添加delay，可以继承QueueScheduler在poll方法加上随机sleep，先观察看看。
-    @GetMapping("/watchWArticles")
-    public String watchWArticles() {
-        log.debug("watchWArticles enter");
-        Map<String, WAccountPO> accountPOMap = wAccountProvider.getNeedFetchAccountMap();
-        Spider spider = Spider.create(wArticleProcessor);
-        for (WAccountPO accountPO : accountPOMap.values()) {
-            String url = "https://mp.weixin.qq.com/cgi-bin/appmsg?action=list_ex&begin=0&count=5&fakeid=" + accountPO.getFakeId() + "&type=9&query=&token=" + wechatConfig.getToken() + "&lang=zh_CN&f=json&ajax=1";
-            Request request = new Request(url);
-            request.addHeader("cookie", wechatConfig.getCookie());
-            request.addHeader("user-agent", wechatConfig.getUserAgent());
-            spider.addRequest(request);
-        }
-//        WAccountPO accountPO = wAccountProvider.findByFakeId("MjM5NjM0MDEwNg==");
-//        String url = "https://mp.weixin.qq.com/cgi-bin/appmsg?action=list_ex&begin=0&count=5&fakeid=" + accountPO.getFakeId() + "&type=9&query=&token=" + wechatConfig.getToken() + "&lang=zh_CN&f=json&ajax=1";
-//        Request request = new Request(url);
-//        request.addHeader("cookie", wechatConfig.getCookie());
-//        request.addHeader("user-agent", wechatConfig.getUserAgent());
-//        spider.addRequest(request);
-        spider.thread(5).start();
-        return "watchWArticles success";
+    @PostMapping("/listenWArticlesUpdate")
+    public String listenWArticlesUpdate() {
+        wechatService.listenWArticlesUpdate();
+        return "listenWArticlesUpdate success";
     }
 
-    @GetMapping("/loadWAccount")
-    public String loadWAccount(@RequestParam("query") String query) {
-        log.debug("loadWAccount : query = {}", query);
-        String encodeQuery = URLEncoder.encode(query, StandardCharsets.UTF_8);
-        String url = "https://mp.weixin.qq.com/cgi-bin/searchbiz?action=search_biz&begin=0&count=5&query=" + encodeQuery + "&token=" + wechatConfig.getToken() + "&lang=zh_CN&f=json&ajax=1";
-        Request request = new Request(url);
-        request.addHeader("cookie", wechatConfig.getCookie());
-        request.addHeader("user-agent", wechatConfig.getUserAgent());
-        Spider.create(wAccountProcessor)
-                .addRequest(request)
-                .thread(5)
-                .run();
-        return "loadWAccount success";
+    @PostMapping("/listenWArticlesUpdateForAccounts")
+    public String listenWArticlesUpdateForAccounts(@RequestParam("accountNames") List<String> accountNames) {
+        wechatService.listenWArticlesUpdate(accountNames);
+        return "listenWArticlesUpdateForAccounts success";
+    }
+
+    @PostMapping("/searchAndInsertWAccount")
+    public String searchAndInsertWAccount(@RequestParam("searchContent") String searchContent) {
+        wechatService.searchAndInsertWAccount(searchContent);
+        return "searchAndInsertWAccount success";
     }
 
     @PostMapping("/updateWAccountNeedToFetchByName")
     public String updateWAccountNeedToFetchByName(@RequestParam("accountNames") List<String> accountNames, @RequestParam("needToFetch") boolean needToFetch) {
-        if (!CollectionUtil.isEmpty(accountNames)) {
-            wechatService.updateWAccountNeedToFetchByName(accountNames, needToFetch);
-        }
+        wechatService.updateWAccountNeedToFetchByName(accountNames, needToFetch);
         return "updateWAccountNeedToFetchByName success";
     }
 
     @PostMapping("/updateWAccountNeedToFetchByFakeId")
     public String updateWAccountNeedToFetchByFakeId(@RequestParam("fakeIds") List<String> fakeIds, @RequestParam("needToFetch") boolean needToFetch) {
-        if (!CollectionUtil.isEmpty(fakeIds)) {
-            wechatService.updateWAccountNeedToFetchByFakeId(fakeIds, needToFetch);
-        }
+        wechatService.updateWAccountNeedToFetchByFakeId(fakeIds, needToFetch);
         return "updateWAccountNeedToFetchByFakeId success";
     }
 
@@ -132,26 +60,14 @@ public class WebMagicController {
     public String findAndInsertWAccount(
             @RequestParam("accountName") String accountName,
             @RequestParam(name = "needToFetch", required = false, defaultValue = "false") boolean needToFetch) {
-        String encodeAccountName = URLEncoder.encode(accountName, StandardCharsets.UTF_8);
-        String url = "https://mp.weixin.qq.com/cgi-bin/searchbiz?action=search_biz&begin=0&count=5&query=" + encodeAccountName + "&token=" + wechatConfig.getToken() + "&lang=zh_CN&f=json&ajax=1";
-        // get请求没有添加header的方法
-        // 使用RestTemplate拿不到list数据，为空，因此使用okhttp。
-        wechatService.findAndInsertWAccount(url, accountName, needToFetch);
-        return null;
+        wechatService.findAndInsertWAccount(accountName, needToFetch);
+        return "findAndInsertWAccount success";
     }
 
     @PostMapping("/updateWechatConfig")
     public String updateWechatConfig(@RequestParam("token") String token, @RequestParam("cookie") String cookie) {
         wechatService.updateWechatProperties(token, cookie);
         return "updateWechatConfig success";
-    }
-
-    public String updateArticleNeedToPublish() {
-        return "";
-    }
-
-    public void loadTotalArticles() {
-
     }
 
     public void sendMessageToAllUser() {
--- src/main/java/com/bear/crawler/webmagic/service/WechatService.java
+++ src/main/java/com/bear/crawler/webmagic/service/WechatService.java
@@ -1,5 +1,6 @@
 package com.bear.crawler.webmagic.service;
 
+import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.map.MapUtil;
 import com.bear.crawler.webmagic.dao.WAccountDao;
@@ -9,6 +10,9 @@ import com.bear.crawler.webmagic.mybatis.generator.po.WAccountPO;
 import com.bear.crawler.webmagic.pojo.WechatConfig;
 import com.bear.crawler.webmagic.pojo.dto.WAccountDto;
 import com.bear.crawler.webmagic.pojo.dto.WAccountsRespDto;
+import com.bear.crawler.webmagic.processor.WAccountProcessor;
+import com.bear.crawler.webmagic.processor.WArticleDetailProcessor;
+import com.bear.crawler.webmagic.processor.WArticleProcessor;
 import com.bear.crawler.webmagic.provider.WArticleProvider;
 import com.bear.crawler.webmagic.provider.WAccountProvider;
 import com.bear.crawler.webmagic.util.OtherUtil;
@@ -16,11 +20,14 @@ import com.bear.crawler.webmagic.util.TransformBeanUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import us.codecraft.webmagic.Request;
+import us.codecraft.webmagic.Spider;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -44,8 +51,72 @@ public class WechatService {
     @Autowired
     private OkHttp okHttp;
 
-    public void updateWAccountNeedToFetchByName(@NonNull List<String> accountNames, boolean needToFetch) {
+    @Autowired
+    private WArticleDetailProcessor wArticleDetailProcessor;
+
+    @Autowired
+    private WArticleProcessor wArticleProcessor;
+
+    @Autowired
+    private WAccountProcessor wAccountProcessor;
+
+    public void fetchWArticleDetail(String query) {
+        Spider.create(wArticleDetailProcessor)
+                .addUrl("https://mp.weixin.qq.com/s/" + query)
+                .thread(5)
+                .run();
+    }
+
+    public void listenWArticlesUpdate() {
+        log.debug("listenWArticlesUpdate: enter");
+        Map<String, WAccountPO> accountPOMap = wAccountProvider.getNeedFetchAccountMap();
+        Spider spider = Spider.create(wArticleProcessor);
+        for (WAccountPO accountPO : accountPOMap.values()) {
+            addRequestToSpider(spider, accountPO);
+        }
+        spider.thread(5).start();
+    }
+
+    public void listenWArticlesUpdate(List<String> accountNames) {
+        log.debug("listenWArticlesUpdate: accountNames = {}", accountNames);
+        Spider spider = Spider.create(wArticleProcessor);
+        for (String accountName : accountNames) {
+            WAccountPO accountPO = wAccountProvider.findByNickname(accountName);
+            if (accountPO == null) {
+                log.info("listenWArticlesUpdate: {} do not exist", accountName);
+            } else {
+                addRequestToSpider(spider, accountPO);
+            }
+        }
+        spider.thread(1).start();
+    }
+
+    private void addRequestToSpider(Spider spider, WAccountPO accountPO) {
+        String url = "https://mp.weixin.qq.com/cgi-bin/appmsg?action=list_ex&begin=0&count=5&fakeid=" + accountPO.getFakeId() + "&type=9&query=&token=" + wechatConfig.getToken() + "&lang=zh_CN&f=json&ajax=1";
+        Request request = new Request(url);
+        request.addHeader("cookie", wechatConfig.getCookie());
+        request.addHeader("user-agent", wechatConfig.getUserAgent());
+        spider.addRequest(request);
+    }
+
+    public void searchAndInsertWAccount(String searchContent) {
+        log.debug("searchAndInsertWAccount : searchContent = {}", searchContent);
+        String encodeQuery = URLEncoder.encode(searchContent, StandardCharsets.UTF_8);
+        String url = "https://mp.weixin.qq.com/cgi-bin/searchbiz?action=search_biz&begin=0&count=5&query=" + encodeQuery + "&token=" + wechatConfig.getToken() + "&lang=zh_CN&f=json&ajax=1";
+        Request request = new Request(url);
+        request.addHeader("cookie", wechatConfig.getCookie());
+        request.addHeader("user-agent", wechatConfig.getUserAgent());
+        Spider.create(wAccountProcessor)
+                .addRequest(request)
+                .thread(1)
+                .run();
+    }
+
+    public void updateWAccountNeedToFetchByName(List<String> accountNames, boolean needToFetch) {
         log.debug("updateWAccountNeedToFetchByName, accountNames = {}, needToFetch = {}", accountNames, needToFetch);
+        if (CollectionUtil.isEmpty(accountNames)) {
+            return;
+        }
         for (String accountName : accountNames) {
             WAccountPO accountPO = wAccountProvider.findByNickname(accountName);
             if (accountPO != null && needToFetch != accountPO.getNeedFetch()) {
@@ -56,8 +127,11 @@ public class WechatService {
         }
     }
 
-    public void updateWAccountNeedToFetchByFakeId(@NonNull List<String> fakeIds, boolean needToFetch) {
+    public void updateWAccountNeedToFetchByFakeId(List<String> fakeIds, boolean needToFetch) {
         log.debug("updateWAccountNeedToFetchByFakeId, fakeIds = {}, needToFetch = {}", fakeIds, needToFetch);
+        if (CollectionUtil.isEmpty(fakeIds)) {
+            return;
+        }
         for (String fakeId : fakeIds) {
             WAccountPO accountPO = wAccountProvider.findByFakeId(fakeId);
             if (accountPO != null && needToFetch != accountPO.getNeedFetch()) {
@@ -68,8 +142,12 @@ public class WechatService {
         }
     }
 
-    public void findAndInsertWAccount(String url, String accountName, boolean needToFetch) {
+    // 使用RestTemplate get请求没有添加header的方法
+    // 使用RestTemplate拿不到list数据，为空，因此使用okhttp。
+    public void findAndInsertWAccount(String accountName, boolean needToFetch) {
         log.debug("findAndInsertWAccount: accountName = {}, needToFetch = {}", accountName, needToFetch);
+        String encodeAccountName = URLEncoder.encode(accountName, StandardCharsets.UTF_8);
+        String url = "https://mp.weixin.qq.com/cgi-bin/searchbiz?action=search_biz&begin=0&count=5&query=" + encodeAccountName + "&token=" + wechatConfig.getToken() + "&lang=zh_CN&f=json&ajax=1";
         WAccountPO existAccountPO = wAccountProvider.findByNickname(accountName);
         if (existAccountPO != null) {
             log.debug("findAndInsertWAccount: exist accountPO, needToFetch = {}", existAccountPO.getNeedFetch());
@@ -78,7 +156,7 @@ public class WechatService {
                 wAccountDao.updateByFakeId(existAccountPO);
                 wAccountProvider.updateNeedToFetchCache(existAccountPO);
             }
-        } else  {
+        } else {
             Map<String, String> headers = MapUtil.builder("Cookie", wechatConfig.getCookie())
                     .put("User-Agent", wechatConfig.getUserAgent()).build();
             WAccountsRespDto accountsRespDto = okHttp.get(url, null, headers, WAccountsRespDto.class);
